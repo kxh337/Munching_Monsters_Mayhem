97aab3dc08ba61f113971d5f779dbfc684cd8793
Kei Hitomi <khitomi337@gmail.com>
2014-02-13 15:56:59 -0500

    Bow and Arrow Added
    Basic Bow and Arrow Scripts created

---
+++ Assets/Weapons/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+	public float damage;
+
+	public static float arrowCount;
+
+	private bool stuck;
+
+	// Use this for initialization
+	void Start () {
+		arrowCount = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//if arrow hits enemy the arrow will get stuck on enemy.
+	//
+	void OnCollisionEnter(Collision collision){
+		//check if collided with enemy
+		if((collision.gameObject.tag == "Enemy")){
+			transform.parent = collision.transform;
+			stuck = true;
+		}
+		//check if collided with player and add arrow count if it did
+		if(collision.gameObject.tag == "Player"){
+			stuck = false;
+			arrowCount++;
+		}
+	}
+}
---
+++ Assets/Weapons/Bow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+	public Arrow arrow;
+
+	private bool charging;
+	private float chargeValue;
+
+	// Use this for initialization
+	void Start () {
+		charging = false;
+		chargeValue = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			chargeShot();
+			shoot();
+	}
+
+	//loads and charges the arrow
+	void chargeShot(){
+		while(Input.GetMouseButtonDown(0) && Arrow.arrowCount > 0){
+			charging = true;
+
+		}
+	}
+
+	//shoots the arrow
+	void shoot(){
+		if(Input.GetMouseButtonUp(1) && charging){
+			charging = false;
+			Arrow.arrowCount--;
+		}
+	}
+
+
+}
