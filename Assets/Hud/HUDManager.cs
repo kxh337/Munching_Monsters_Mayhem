2ea024849076fdcc195b0a358fb1d649b55a6013
Hun Jae Lee <hunjaelee@icloud.com>
2014-02-15 02:41:44 -0500

    Added HUD
    Added health, fuel, and arrows -Hun Jae

---
+++ Assets/Hud/HUDManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour {
+
+	public float health;
+	public float fuel;
+	public float arrows;
+	public bool lanternOn;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		fuel = 100;
+		arrows = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lanternOn == true && fuel > 0) {
+			useFuel ();
+		} 
+		else {
+		// turn off the directional light
+		}
+	}
+
+	void useFuel () {
+		fuel -= Time.deltaTime;
+	}
+
+
+}
