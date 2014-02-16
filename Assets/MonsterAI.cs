9f456759503f157d2bf4bedacb277930f4c8f208
fenru <saw106@case.edu>
2014-02-16 11:47:13 -0500

    AI Update
    Beginning of AI should turn monsters to face the player(might need a
    minus sign in the math for certain cases), however the Muktar monsters
    don't react to rotation commands.  The AI script also won't find the
    player object I told it to find.  Currently, the AI script is making
    monsters disappear for testing purposes.

---
+++ Assets/MonsterAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAI : MonoBehaviour {
+
+	private bool hunt = false;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hunt) {
+			//gameObject.transform.rotation.Set(gameObject.transform.rotation.x, Mathf.Atan((player.transform.position.y - gameObject.transform.position.y) / (player.transform.position.x - gameObject.transform.position.x)), gameObject.transform.rotation.z, gameObject.transform.rotation.w);		
+			Destroy(gameObject);
+		}
+		else {
+		}
+	}
+
+	void OnTriggerEnter() {
+		hunt = true;
+	}
+
+	void OnTriggerExit() {
+		hunt = false;
+	}
+}
